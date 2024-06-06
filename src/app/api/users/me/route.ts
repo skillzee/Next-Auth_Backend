e75d39dc45bc8ec921161b44edb27294b2dfa139
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-06-06 16:16:15 +0530

    /me

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,23 @@
+import { connectDb } from "@/dbConnect/dbConnect";
+import User from '@/models/userModel'
+import {NextRequest, NextResponse} from 'next/server'
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+
+
+connectDb();
+
+
+export async function POST(request: NextRequest) {
+    try {
+        // Extract data from token
+        const userId = await getDataFromToken(request)
+        const user = User.findOne({_id: userId}).select("-password")
+
+        return NextResponse.json({
+            message: "User Found",
+            data: user
+        })
+    } catch (error) {
+        
+    }
+}
