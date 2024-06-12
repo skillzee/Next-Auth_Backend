5cbe145cef2e57b40612082f9471efee419a6256
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-06-12 16:29:23 +0530

    profile

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function page({params}: any) {
+  return (
+    <div className=" flex flex-col items-center justify-center min-h-screen py-2">
+        <h1>Profile Page</h1>
+        <h2 className=" p-3 bg-green-500  rounded">{params.id}</h2>
+    </div>
+  )
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,49 @@
 'use client'
-import React from 'react'
+import axios from "axios"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import toast, { Toast } from "react-hot-toast"
 
-function page() {
+
+export default function ProfilePage() {
+    const router = useRouter()
+    const [data, setdata] = useState("nothing")
+
+    const getUserDetails = async () =>{
+        const res = await axios.post("/api/users/me")
+        console.log(res.data);
+        setdata(res.data.data._id)
+        
+    }
+
+    const logout = async () =>{
+        try {
+            await axios.get('/api/users/logout')
+            toast.success("Logout Success")
+            router.push("/login")
+        } catch (error: any) {
+            console.log(error.message);
+            toast.error(error.message)
+            
+            
+        }
+    }
   return (
-    <div>page</div>
+    <div className=" flex flex-col items-center justify-center min-h-screen py-2">
+        <h1>Profile Page</h1>
+        <hr />
+        <h2>
+            {data === "nothing" ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link>}
+        </h2>
+        <hr />
+        <button
+        className=" rounded bg-red-500 font-bold text-white"
+         onClick={logout}>Logout</button>
+        <button
+        className=" rounded mt-10 bg-blue-500 font-bold text-white"
+         onClick={getUserDetails}>Details</button>
+    </div>
   )
 }
 
-export default page
